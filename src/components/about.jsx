fe2b36d57e197e473dda87792688bd216e4e8319
DenverEllis <ellis.denver@yahoo.com>
2020-12-29 15:32:47 -0600

    Started making changes and testing workflow

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,10 +11,10 @@ export default class About extends Component {
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                 <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
+                    <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I am a undergraduate student at UA Little Rock studying Computer Science. I hold learning to be a passion, and computer science is a fantastic field that can allow me to apply learnings from many disciplines. </p>
+                    <p>My current learning interests are in the realm of cyber security and graphics programming. </p>
                     </div>
                 </div>
                 </div>
@@ -22,6 +22,7 @@ export default class About extends Component {
             </div>
         </div>
         </section>
+        
         <section className="colorlib-about">
         <div className="colorlib-narrow-content">
             <div className="row">
