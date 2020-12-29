1921546b368998e313a6e77bc586937b99ddff28
DenverEllis <ellis.denver@yahoo.com>
2020-12-29 15:31:38 -0600

    Updates

---
+++ precache-manifest.f20e306ae2fa0a8cab571d117653d227.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cf3aa58ac7a6e1d7c2c",
+    "url": "/me/static/css/main.adc14dd0.chunk.css"
+  },
+  {
+    "revision": "6cf3aa58ac7a6e1d7c2c",
+    "url": "/me/static/js/main.6cf3aa58.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "3ab4cdf77cd00a4b46d791fdbc828540",
+    "url": "/me/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85c0f77321db069b866f86e15f05a927.js"
+  "/me/precache-manifest.f20e306ae2fa0a8cab571d117653d227.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/me/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
